af3c9380646042571ceb71537b3f3afbbf6831d1
El Algoritmo de la Noche <44403136+ElAlgoritmoDeLaNoche@users.noreply.github.com>
2020-11-29 20:20:50 -0600

    Menú de navegación creado en HTML CSS y JS

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// Menu de Navegación
+let bar_icon = document.querySelector('.bar-icon');
+let nav = document.querySelector('.nav');
+let enlaces = document.querySelector('.container-list');
+let contador = true;
+
+bar_icon.addEventListener('click', function() {
+    this.classList.toggle('active');
+
+    if (contador == true) {
+        enlaces.style.width = '100%';
+        enlaces.style.overflow = 'hidden';
+        enlaces.style.transition = '0.5s';
+
+        nav.style.background = '#f2bb26';
+        nav.style.transition = '0.5s';
+
+        contador = false;
+    } else {
+        contador = true;
+        enlaces.style.width = '0%';
+        enlaces.style.overflow = 'hidden';
+        enlaces.style.transition = '0.5s';
+
+        nav.style.background = 'transparent';
+        nav.style.transition = '0.5s';
+    }
+});
